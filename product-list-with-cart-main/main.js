32f5c5806e6e4633d72aa64d63376b4ab5552fc5
Al <msayev02@gmail.com>
2025-09-25 22:56:48 +0400

    Cleanup the mainjs file

--- product-list-with-cart-main/main.js
+++ product-list-with-cart-main/main.js
@@ -1,55 +1,59 @@
 import { fetchProducts, renderPrice } from "./products.js";
 import { setupAddToCartButtons } from "./buttonEvents.js";
 import { setProducts } from "./cart.js";
+
 const grid = document.querySelector(".grid");
 
-grid.innerHTML = `<p class="loading">Loading products...</p>`;
-
 async function init() {
-  const products = await fetchProducts();
+  try {
+    const products = await fetchProducts();
 
-  if (products.length === 0) {
-    grid.innerHTML = `<p class="error">Failed to load products. Please try again later.</p>`;
-    return;
-  }
+    if (!products || products.length === 0) {
+      grid.innerHTML = `<p class="error">Failed to load products. Please try again later.</p>`;
+      return;
+    }
 
-  // set products in cart module
-  setProducts(products);
+    // Set products in cart module
+    setProducts(products);
 
-  let gridHTML = "";
-
-  products.forEach((product) => {
-    gridHTML += `
+    const gridHTML = products
+      .map(
+        (product) => `
       <div class="product-box">
-          <div class="product-box__image-n-button">
-              <img src="${product.image.mobile}" alt="${
-      product.category
-    }" class="product-box__image-mobile">
-              <img src="${product.image.tablet}" alt="${
-      product.category
-    }" class="product-box__image-tablet">
-              <img src="${product.image.desktop}" alt="${
-      product.category
-    }" class="product-box__image-desktop">
-              <button class="product-box__button js-product-box-button hoverOn" data-product-id='${
-                product.id
-              }'>
-                  <img src="./assets/images/icon-decrement-quantity.svg" class="decrement-quantity" data-quantity>
-                  <img src="./assets/images/icon-add-to-cart.svg" class="cart-image">
-                  <span class="button-text">Add to Cart</span>
-                  <img src="./assets/images/icon-increment-quantity.svg" class="increment-quantity" data-quantity>
-              </button>
-          </div>
-          <p class="product-box__short-name">${product.category}</p>
-          <p class="product-box__full-name">${product.name}</p>
-          <p class="product-box__price">$${renderPrice(product.price)}</p> 
+        <div class="product-box__image-n-button">
+          <img src="${product.image.mobile}" alt="${
+          product.category
+        }" class="product-box__image-mobile">
+          <img src="${product.image.tablet}" alt="${
+          product.category
+        }" class="product-box__image-tablet">
+          <img src="${product.image.desktop}" alt="${
+          product.category
+        }" class="product-box__image-desktop">
+          <button class="product-box__button js-product-box-button hoverOn" data-product-id='${
+            product.id
+          }'>
+            <img src="./assets/images/icon-decrement-quantity.svg" class="decrement-quantity" data-quantity>
+            <img src="./assets/images/icon-add-to-cart.svg" class="cart-image">
+            <span class="button-text">Add to Cart</span>
+            <img src="./assets/images/icon-increment-quantity.svg" class="increment-quantity" data-quantity>
+          </button>
+        </div>
+        <p class="product-box__short-name">${product.category}</p>
+        <p class="product-box__full-name">${product.name}</p>
+        <p class="product-box__price">$${renderPrice(product.price)}</p>
       </div>
-      `;
-  });
-  grid.innerHTML = gridHTML;
+    `
+      )
+      .join("");
 
-  // rebind button logic
-  setupAddToCartButtons();
+    grid.innerHTML = gridHTML;
+
+    setupAddToCartButtons();
+  } catch (error) {
+    console.error("Error initializing products:", error);
+    grid.innerHTML = `<p class="error">Something went wrong while loading products.</p>`;
+  }
 }
 
 init();
